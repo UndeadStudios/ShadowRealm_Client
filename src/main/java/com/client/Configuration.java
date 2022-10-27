004c04abfc4b0edc8d7d89e95551bffb38403299
ArclightEpic <94341027+ArclightEpic@users.noreply.github.com>
2022-10-27 08:55:10 -0700

    client version 47

--- src/main/java/com/client/Configuration.java
+++ src/main/java/com/client/Configuration.java
@@ -15,7 +15,7 @@ public class Configuration {
 	 * Cache version is written to the cache folder inside a version file.
 	 * This is read on startup to tell if the cache is out of date or not.
 	 */
-	public static final int CACHE_VERSION = 46; // Set this to the same value, v0.030 = 30, v1.120 = 1120
+	public static final int CACHE_VERSION = 47; // Set this to the same value, v0.030 = 30, v1.120 = 1120
 
 	public static final String CACHE_LINK = "https://cache.exilius-osrs.com/v0.049.zip";
 
